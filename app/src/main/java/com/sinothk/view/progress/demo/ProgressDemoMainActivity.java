3c963595dab82e9b763999ad3911e9f1f190650f
LiangYT <381518188@qq.com>
2018-12-21 13:34:23 +0800

    同步

--- app/src/main/java/com/sinothk/view/progress/demo/ProgressDemoMainActivity.java
+++ app/src/main/java/com/sinothk/view/progress/demo/ProgressDemoMainActivity.java
@@ -1,7 +1,7 @@
 package com.sinothk.view.progress.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ProgressDemoMainActivity extends AppCompatActivity {
 
@@ -9,5 +9,9 @@ public class ProgressDemoMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_demo_main);
+
+//        HiveProgressView progressView = (HiveProgressView) findViewById(R.id.hive_progress);
+//        progressView.setRainbow(false);
+//        progressView.setColor(0x000000);
     }
 }
