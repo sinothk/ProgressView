f3f10c2651824d999a82d6cf56b18d0dafc846ba
LiangYT <381518188@qq.com>
2018-12-21 11:36:25 +0800

    同步

---
+++ app/src/main/java/com/sinothk/view/progress/demo/ProgressDemoMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.view.progress.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProgressDemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_demo_main);
+    }
+}
